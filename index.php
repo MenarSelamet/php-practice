d7fabf9a9667448f5ac8f46b24a8d5e7d2fb52db
Menar Selamet <menarselamet007@gmail.com>
2024-07-19 13:57:05 +0300

    basics

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php  
+// I'm going to conquer the world
+    echo"I love Pizza so much <br>";
+    echo"With extra cheese <br>";
+
+    $name="MAMA MIA";
+    echo $name;
+    $food=" you know it ";
+
+    echo"<br> This is going to take some long time {$name}";
+    echo "<br> You know what's on my mind {$food}";
+?>
+
