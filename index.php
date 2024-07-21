e3c7baf2dfdb270980269b5f7c8ac0f3b6142150
Menar Selamet <menarselamet007@gmail.com>
2024-07-21 14:29:28 +0300

    practice

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<?php  
-// I'm going to conquer the world
-    echo"I love Pizza so much <br>";
-    echo"With extra cheese <br>";
+<!-- <?php
+        // I'm going to conquer the world
+        echo "I love Pizza so much <br>";
+        echo "With extra cheese <br>";
 
-    $name="MAMA MIA";
-    echo $name;
-    $food=" you know it ";
+        $name = "MAMA MIA";
+        echo $name;
+        $food = " you know it ";
 
-    echo"<br> This is going to take some long time {$name}";
-    echo "<br> You know what's on my mind {$food}";
+        echo "<br> This is going to take some long time {$name}";
+        echo "<br> You know what's on my mind {$food}";
+        ?> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <label> Username: </label><br>
+        <input type="text" name="username" /> <br>
+        <label> Password: </label><br>
+        <input type="password" name="password" />
+
+    </form>
+</body>
+
+</html>
+
+<?php
+echo "Hello World";
+
 ?>
-
