58a1c6f50a026c60279ab9b74506ae6e2afc51c1
Menar Selamet <menarselamet007@gmail.com>
2024-08-12 18:49:00 +0300

    Making data dynamic

--- Database.php
+++ Database.php
@@ -4,28 +4,16 @@ class Database {
 
 public $connection;
 
-public function __construct() {
-
-    $config = [
-        "host"=> 'localhost',
-        'port'=> '3306',        
-        'dbname'=> 'myapp',
-        'charset'=> 'utf8mb4',
-    ];
+public function __construct($config, $username = 'root', $password = '') {
 
     
 
 // dumpAndDie($dsn = 'mysql:' . http_build_query( $config, ' ', ';' ));
 $dsn = 'mysql:' . http_build_query( $config, ' ', ';' );
 
-$this -> connection = new PDO($dsn, 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+$this -> connection = new PDO($dsn, $username, $password, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
 }
 public function query ($query) {
-
-
-
-
-
 
 $statement = $this-> connection->prepare($query);
 $statement->execute();
@@ -33,15 +21,4 @@ return $statement;
 }
 }
 
-
-
-
 // dumpAndDie($posts);
-
-$db = new Database();
-$posts = $db ->query('select id, title from untitled_table_1 ')->fetchAll();
-
-foreach ($posts as $post) {
-echo "<li>" . $post['id'] . "</li>";
-echo "<li>" . $post['title'] . "</li>";
-};
--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 require 'functions.php';
 // require 'router.php';
 require 'Database.php';
+$config = require 'config.php';
+
+
+$db = new Database($config,'root', '' );
+$posts = $db ->query('select id, title from untitled_table_1 ')->fetchAll();
+
+foreach ($posts as $post) {
+echo "<li>" . $post['id'] . "</li>";
+echo "<li>" . $post['title'] . "</li>";
+};
