03cf32d964253098ae14f3ac8543ba769934b790
Menar Selamet <menarselamet007@gmail.com>
2024-08-12 18:50:58 +0300

    changing config file

--- config.php
+++ config.php
@@ -2,8 +2,12 @@
 
 
     return [
+        "database"=> [
         "host"=> 'localhost',
         'port'=> '3306',        
         'dbname'=> 'myapp',
-        'charset'=> 'utf8mb4',
+        'charset'=> 'utf8mb4'
+    ],
+
+    
     ];
