266a78659db6938cad68cf6b234f5888b2a0fe06
Menar Selamet <menarselamet007@gmail.com>
2024-08-13 18:39:22 +0300

    Adding validation to number of characters allowed

--- controllers/note-form.php
+++ controllers/note-form.php
@@ -14,6 +14,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' ){
         $errors['body'] = 'You have to fill this area!!';
 
     };
+     if (strlen($_POST['body']) > 15 ){
+        $errors['body'] = 'The body can not be more than 15 character!';
+
+    };
         if (empty($errors)){
         $db ->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',[
         'body'=> htmlspecialchars($_POST['body']),
