803e8cf07d09f96a94c9562f4712145d79a88220
Menar Selamet <menarselamet007@gmail.com>
2024-08-13 11:22:07 +0300

    Creating a note view

--- controllers/note.php
+++ controllers/note.php
@@ -3,6 +3,6 @@ $heading = "Single Note";
 
 $config = require 'config.php';
 $db = new Database($config["database"],'root', '' );
-$notes = $db->query("select * from notes where id = :id", ['id' => $_GET["id"]])->fetchAll();
+$note = $db->query("select * from notes where id = :id", ['id' => $_GET["id"]])->fetch();
 
-require "views/notes.view.php";
+require "views/note.view.php";
---
+++ views/note.view.php
@@ -0,0 +1,20 @@
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
+        <?php require "partials/nav.php"; ?>
+        <?php require "partials/head.php"; ?>
+        <?php require "partials/main.php"; ?>
+        <p><?= $note['body'] ?></p>
+    </div>
+
+</body>
+
+</html>
