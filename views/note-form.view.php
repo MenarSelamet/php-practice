892d866731d2a3e8680152a150a91ffd913f843e
Menar Selamet <menarselamet007@gmail.com>
2024-08-13 18:49:03 +0300

    Adding a null coalescing operator

--- views/note-form.view.php
+++ views/note-form.view.php
@@ -21,7 +21,7 @@
                                 here:</label>
                             <div class="mt-2">
                                 <textarea id="body" name="body" rows="3"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= $_POST['body'] ?? '' ?></textarea>
                                 <?php if (isset($errors['body'])) : ?>
                                 <p class="text-red-600 text-sm "><?php echo $errors['body']; ?></p>
                                 <?php endif; ?>
