d30d5d18ccebe331fe7e4f18a17274313b957476
Menar Selamet <menarselamet007@gmail.com>
2024-08-14 13:14:09 +0300

    Moving core files to core folder

--- core/router.php
+++ core/router.php
@@ -5,18 +5,18 @@
 $currentURI = parse_url($_SERVER["REQUEST_URI"])['path'];
 $baseURI = "/php-practice";
 echo "Current URI: " . $currentURI . "<br>";
-$routes = require ('routes.php');
+$routes = require base_path('routes.php');
 
 function abort($code = 404) {   
     http_response_code($code);
-     require "views/{$code}.php";
+     require base_path("views/{$code}.php");
      die(); 
 }
 function routeToController ($currentURI, $routes) { 
      $clicked = "rounded-md bg-gray-900 text-white";
     $notClicked = "tex-gray-300";
     if (array_key_exists($currentURI, $routes)) {
-    require $routes[$currentURI]; 
+    require base_path($routes[$currentURI]); 
 } else {
     abort();
  };
