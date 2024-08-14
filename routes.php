36cec68ca320d4231bcfb5b7552f5382462864dc
Menar Selamet <menarselamet007@gmail.com>
2024-08-14 23:18:26 +0300

    Huge bug with single note page

--- routes.php
+++ routes.php
@@ -6,8 +6,10 @@ $router->get('/php-practice/contact', 'controllers/contact.php');
 
 $router->get('/php-practice/notes', 'controllers/notes/index.php');
 $router->get('/php-practice/note', 'controllers/notes/show.php');
-$router->delete('/php-practice/note', 'controllers/notes/delete.php');
+$router->delete('/php-practice/note', 'controllers/notes/destroy.php');
+
 $router->get('/php-practice/notes/create', 'controllers/notes/create.php');
+$router->post('/php-practice/notes', 'controllers/notes/store.php');
 
 // return [
 //     $baseURI . "/"=>  "controllers/index.php",
