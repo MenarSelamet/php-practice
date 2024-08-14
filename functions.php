fa351f1804c8b2f2c06923b89c0846ea1c0f91ed
Menar Selamet <menarselamet007@gmail.com>
2024-08-14 11:02:11 +0300

    Creating a function for the views

--- functions.php
+++ functions.php
@@ -23,4 +23,12 @@ echo "</pre";
 function base_path($path)
 {
     return BASE_PATH . $path;
+};
+
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+
+    require base_path('views/' . $path);
 }
