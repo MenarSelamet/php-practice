535304d4708ffcccb105940e20da19956d534479
Menar Selamet <menarselamet007@gmail.com>
2024-08-14 10:57:40 +0300

    Creating a function for Paths

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 const BASE_PATH = __DIR__.'/../';
 
 
-require BASE_PATH. 'functions.php';
-require BASE_PATH. 'Response.php';
-require BASE_PATH. 'Database.php';
-require BASE_PATH. 'router.php';
+require BASE_PATH.'functions.php';
+require base_path('Response.php');
+require base_path('Database.php');
+require base_path('router.php');
