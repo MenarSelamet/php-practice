348fdff4446264cb8413d9cd2644546e5fcc59c1
Menar Selamet <menarselamet007@gmail.com>
2024-08-14 16:55:40 +0300

    Changing routes to have different methods

--- core/router.php
+++ core/router.php
@@ -1,25 +1,55 @@
 <?php
+namespace Core;
+class Router {
+    protected $routes = [];
 
+     public function add($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method
+        ];
+    }
 
-
-$currentURI = parse_url($_SERVER["REQUEST_URI"])['path'];
-$baseURI = "/php-practice";
-echo "Current URI: " . $currentURI . "<br>";
-$routes = require base_path('routes.php');
-
-function abort($code = 404) {   
-    http_response_code($code);
-     require base_path("views/{$code}.php");
-     die(); 
+    public function get($uri, $controller)
+    {
+        $this->add('GET', $uri, $controller);
+    }
+     public function post ($uri, $controller){
+        $this -> add('POST', $uri, $controller);
+    }
+     public function delete ($uri, $controller){
+        $this -> add ('DELETE', $uri, $controller);
+    }
+     public function patch ($uri, $controller){
+        $this -> add('PATCH', $uri, $controller);
+    }
+    public function put ($uri, $controller){
+        $this -> add('PUT', $uri, $controller);
+    }
 }
-function routeToController ($currentURI, $routes) { 
-     $clicked = "rounded-md bg-gray-900 text-white";
-    $notClicked = "tex-gray-300";
-    if (array_key_exists($currentURI, $routes)) {
-    require base_path($routes[$currentURI]); 
-} else {
-    abort();
- };
-  }
 
-routeToController($currentURI, $routes);
+
+
+// $currentURI = parse_url($_SERVER["REQUEST_URI"])['path'];
+// $baseURI = "/php-practice";
+// echo "Current URI: " . $currentURI . "<br>";
+// $routes = require base_path('routes.php');
+
+// function abort($code = 404) {   
+//     http_response_code($code);
+//      require base_path("views/{$code}.php");
+//      die(); 
+// }
+// function routeToController ($currentURI, $routes) { 
+//      $clicked = "rounded-md bg-gray-900 text-white";
+//     $notClicked = "tex-gray-300";
+//     if (array_key_exists($currentURI, $routes)) {
+//     require base_path($routes[$currentURI]); 
+// } else {
+//     abort();
+//  };
+//   }
+
+// routeToController($currentURI, $routes);
